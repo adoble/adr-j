56c3b37c8d7f746fa21b87722f1490bf3a66fc22
Andrew Doble <andrew.l.doble@gmail.com>
2017-07-21 15:28:17 +0200

    Changed JAR name

--- launch-scripts/adr.bat
+++ launch-scripts/adr.bat
@@ -1,4 +1,3 @@
 @echo off
 
-java -jar %ADR_HOME%\target\adr-0.0.1-SNAPSHOT-jar-with-dependencies.jar %*
-REM java -jar %ADR_HOME%\target\adr-0.0.1-SNAPSHOT.jar %*
+java -jar %ADR_HOME%\target\adr-j-0.0.1-SNAPSHOT-jar-with-dependencies.jar %*
