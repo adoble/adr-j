28aa3ff87ca5d2cf36c334f3a8ab169a33a9460d
Torsten Kleiber <torsten.kleiber@web.de>
2022-03-31 15:55:15 +0200

    Fix #39 hard reference to *.md glob prevents generation of file links for other templates like AsciiDoc

--- src/main/java/org/doble/adr/ADRFilter.java
+++ src/main/java/org/doble/adr/ADRFilter.java
@@ -3,37 +3,36 @@ import java.nio.file.*;
 import java.util.function.Predicate;
 
 public class ADRFilter {
-	
+
 
 	public static Predicate<Path> filter() {
 
-		return p -> { 
-			String glob = "[0-9][0-9][0-9][0-9]*.md";   //TODO remove the  search for only markdown files
-			                                            // TODO test for other file types!
+		return p -> {
+			String glob = "[0-9][0-9][0-9][0-9]*.*"; // TODO test for other file types!
 			return p.getFileSystem().getPathMatcher("glob:" + glob).matches(p.getName(p.getNameCount()-1));
 		};
 
 	}
-	
-	public static Predicate<Path> filter(String adrID) {  //TODO remove the  search for only markdown files
 
-		return p -> { 
-			String glob = adrID + "*.md";
+	public static Predicate<Path> filter(String adrID) {
+
+		return p -> {
+			String glob = adrID + "*.*";
 			return p.getFileSystem().getPathMatcher("glob:" + glob).matches(p.getName(p.getNameCount()-1));
-			
+
 		};
 
 	}
-	
+
 	public static Predicate<Path> filter(int adrID) {
-				return p -> { 
+				return p -> {
 			String formattedID = String.format("%04d", adrID);
-			String glob = formattedID + "*.md";
+			String glob = formattedID + "*.*";
 			return p.getFileSystem().getPathMatcher("glob:" + glob).matches(p.getName(p.getNameCount()-1));
-			
+
 		};
 
 	}
 
-	
+
 }
